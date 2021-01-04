d2fb37dfa41d446f1b932e11c894440d87425962
Mat Dupont <dupont.mat.l@gmail.com>
2021-01-04 11:13:15 -0500

    Created a new Expo app

---
+++ src/screens/signup-screen.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const SignupScreen = () => {
+  return (
+    <View>
+      <Text>Signup Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export { SignupScreen }
---
+++ src/screens/tracklist-screen.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const TrackListScreen = () => {
+  return (
+    <View>
+      <Text>TrackList Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export { TrackListScreen }
